c1ecbe5cf51f02bb110a9fba01597ed94f624a36
ArnabBiswas2303 <arnabdbst.99@gmail.com>
2019-11-11 03:39:53 +0530

    Adding Footer And PopUp

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+  let button = document.getElementById("popUp");
+  let popUp = document.querySelector(".popUpBg");
+  button.addEventListener("click", event => {
+    popUp.style.visibility = "visible";
+  });
+  popUp.addEventListener("click", event => {
+    if (event.target == event.currentTarget) popUp.style.visibility = "hidden";
+  });
+});
